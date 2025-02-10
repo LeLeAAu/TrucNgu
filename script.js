e72588df23092d5408afafd36e846f7324671c67
LeLeAAu <168399108+LeLeAAu@users.noreply.github.com>
2025-02-10 17:43:18 +0700

    Add files via upload
    Version 1 lol

---
+++ script.js
@@ -0,0 +1,27 @@
+
+const messages = [
+    "Đùa thôi đúng k 🥹",
+    "Thằng cụ nhà mày",
+    "Nói thật đi",
+    ":))))))))))))",
+    "Nhìn cái mặt mày kìa",
+    "?:))",
+    "Thế thôi t đ hỏi nữa",
+    "Thôi nào bấm yes đi:)))))))))))",
+    ":))))))))))))))"
+];
+
+let messageIndex = 0;
+
+function handleNoClick() {
+    const noButton = document.querySelector('.no-button');
+    const yesButton = document.querySelector('.yes-button');
+    noButton.textContent = messages[messageIndex];
+    messageIndex = (messageIndex + 1) % messages.length;
+    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+    yesButton.style.fontSize = `${currentSize * 1.525}px`;
+}
+
+function handleYesClick() {
+    window.location.href = "MatThangTruc.html";
+}
