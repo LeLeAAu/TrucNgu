88384d3eafbbac8aa1ec7f77008645b3a1f84bc0
LeLeAAu <168399108+LeLeAAu@users.noreply.github.com>
2025-02-10 18:14:37 +0700

    Update script.js
    :))))

--- script.js
+++ script.js
@@ -8,20 +8,15 @@ const messages = [
     "?:))",
     "Thế thôi t đ hỏi nữa",
     "Thôi nào bấm yes đi:)))))))))))",
-    ":))))))))))))))"
-];
-
+    ":))))))))))))))"];
 let messageIndex = 0;
-
 function handleNoClick() {
     const noButton = document.querySelector('.no-button');
     const yesButton = document.querySelector('.yes-button');
     noButton.textContent = messages[messageIndex];
     messageIndex = (messageIndex + 1) % messages.length;
     const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
-    yesButton.style.fontSize = `${currentSize * 1.525}px`;
-}
-
+    yesButton.style.fontSize = `${currentSize * 1.525}px`;}
 function handleYesClick() {
     window.location.href = "MatThangTruc.html";
 }
